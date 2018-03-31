5da56d8f88daa5a3667c01cc65f4ddf9c6e60814
Pratik Kinage <37847256+thirstycode@users.noreply.github.com>
2018-04-01 00:11:08 +0530

    comment

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 usernames=["kriti.sanon143","urvashirautela26","disha.patani.updates","tamannaah.143","shraddha.kapoor.sweet","manushi143","manushi.chillar.insta","realshrutihaasan"]
+# also edit usernames in export.csv
